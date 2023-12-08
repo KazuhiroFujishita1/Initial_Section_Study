070409187f9d8558189884fe151ac9407bd8bcf1
kazuhiro_fujishita <64386199+KazuhiroFujishita1@users.noreply.github.com>
2023-12-08 19:39:56 +0900

    Add files via upload

--- output_section_data.py
+++ output_section_data.py
@@ -9,10 +9,12 @@ def output_section_data(columns,beams,beam_select_mode):
     with open(output_file + '_'+ str(beam_select_mode)+ '.csv', mode='w',newline='',encoding='utf-8') as file:
         writer = csv.writer(file)
         writer.writerow(['<selected_beam_section>'])
-        writer.writerow(['No','H','B','t1','t2','I','Z','Zp','F'])
+        writer.writerow(['No','H','B','t1','t2','I','Z','Zp','F','ML','MS','QL','QS'])
         for i in beams:
-            writer.writerow([i.no,i.H,i.B,i.t1,i.t2,i.I,i.Z,i.Zp,i.F])
+            writer.writerow([i.no,i.H,i.B,i.t1,i.t2,i.I,i.Z,i.Zp,i.F,i.ML,i.Ms,i.QL,i.Qs])
         writer.writerow(['<selected_column_section>'])
-        writer.writerow(['No','H','t','A','Ix','Iy','Z','F'])
+        writer.writerow(['No','H','t','A','Ix','Iy','Z','F',
+                         'NL','NSx','NSy','MLx','MLy','MSx','MSy','QLx','QLy','QSx','QSy'])
         for i in columns:
-            writer.writerow([i.no,i.H,i.t,i.A,i.Ix,i.Iy,i.Z,i.F])
+            writer.writerow([i.no,i.H,i.t,i.A,i.Ix,i.Iy,i.Z,i.F,
+                             i.NL,i.NSx,i.NSy,i.MLx,i.MLy,i.MSx,i.MSy,i.QLx,i.QLy,i.QSx,i.QSy])
