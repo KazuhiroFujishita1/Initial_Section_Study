9c661008c6c4d64965d19c492ab68913f711a102
kazuhiro_fujishita <64386199+KazuhiroFujishita1@users.noreply.github.com>
2024-03-28 10:59:58 +0900

    Add files via upload

--- RESP_batch.py
+++ RESP_batch.py
@@ -6,12 +6,12 @@ def RESP_batch(resp_path,resp_model_create,resp_opt):
     # コマンドを実行
     if resp_model_create == "Activate":#モデル作成のみする場合
         if resp_opt == "NonActivate":
-            command = fr'{resp_path} -I "3F例題モデル_略算床荷重.dz" --generate "generator_case1.json"'
+            command = fr'{resp_path} -I "initial_model.dz" --generate "generator_case1.json"'
             subprocess.run(command, shell=True)
             print("バッチ処理が正常に完了しました。")
 
     if resp_opt == "Activate":#断面算定処理を行う場合
-        command = fr'{resp_path} -I "3F例題モデル_略算床荷重.dz" --generate "generator_case1.json" --optimize'
+        command = fr'{resp_path} -I "initial_model.dz" --generate "generator_case1.json" --optimize'
         subprocess.run(command, shell=True)
         print("バッチ処理が正常に完了しました。")
 
